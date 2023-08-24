429d8f6dc28d9949ce5a30d48792c4a2b3bad76d
XhOwais <xhowais@gmail.com>
2023-08-24 15:58:13 +0500

    Update

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -29,6 +29,44 @@ const carInfo = {
   price: 7000,
 };
 
+const vehicleSpecifications = [
+  {
+    category: "topSpeed",
+    values: [
+      { value: "169 MPH" },
+    ],
+    valuesInSI: [
+      { value: "272 KM/H" }]
+  },
+  {
+    category: "power",
+    values: [
+      { value: "162 HP" }
+    ],
+    valuesInSI: [
+      { value: "9,250 RPM" }]
+  },
+  {
+    category: "torque",
+    values: [
+      { value: "130.5 N.M" },
+    ],
+    valuesInSI: [
+      { value: "96.3 LBF.FT" }
+    ],
+  },
+  {
+    category: "fuelCapacity",
+    values: [
+      { value: "17 L" },
+    ],
+    valuesInSI: [
+      { value: "3.7 IMP CAL" }
+    ],
+  }
+];
+
+
 export default function Main() {
   const [activeSection, setActiveSection] = useState('Model');
 
@@ -56,39 +94,59 @@ export default function Main() {
 
   return (
     <>
-      <div className=' h-screen w-full flex flex-col relative overflow-x-hidden'>
+      <div className=' h-screen w-full flex flex-col relative px-60 '>
         <div className=''>
           <Navbar />
-        <div>
-        <ul className=" ">
-            {infoList.map((item, index) => (
-              <a
-                className='flex h-auto items-center'
-                key={index}
-                href=""
-                onClick={(e) => handleLinkClick(e, item.heading)}
-              >
-                <li className="py-12 px-12 mr-6 rounded-full bg-slate-100 text-white flex justify-center items-center">
-                  <img className='w-24' src={item.icon} alt="" />
-                </li>
-              </a>
-            ))}
-          </ul>
+          
         </div>
-        </div>
-        <div className="main-container flex flex-col items-center">
-        <div className=" w-847 translate-y-16">
-          <h4 className=' font-sub text-64 -mb-36'>{carInfo.year}</h4>
-          <h1 className=' font-heading text-64 '>{carInfo.make} {carInfo.model}</h1>
-          <h4 className=' font-sub text-64 float-right -mt-36'>{carInfo.variant}</h4>
-        </div>
-        <div className=' w-full flex justify-center items-start relative'>
-        <div className="">
-          <h4 className=' text-299'>{carInfo.yaer}</h4>
-        </div>
-          <ModelViewer />
-          <div className=" w-500 h-500 bg-first rounded-full absolute -z-10"></div>
-        </div>
+        <div className=' flex items-center order-2 absolute bottom-4 '>
+            <ul className="  ">
+              {infoList.map((item, index) => (
+                <a
+                  className='flex h-auto items-center mb-24'
+                  key={index}
+                  href=""
+                  onClick={(e) => handleLinkClick(e, item.heading)}
+                >
+                  <li className="py-12 px-12 mr-6 rounded-full bg-slate-100 text-white flex justify-center items-center">
+                    <img className='w-24' src={item.icon} alt="" />
+                  </li>
+                </a>
+              ))}
+            </ul>
+            <div className=' h-96 w-auto flex  justify-between'>
+              {vehicleSpecifications.map(category => (
+                <div className=' px-42' key={category.category}>
+                  <h2 className=' font-heading text-16 '>{category.category}</h2>
+                  <ul>
+                    {category.values.map(value => (
+                      <li className=' font-body font-bold text-32 text-hover -mt-12' key={value.unit}>
+                        {value.value}
+                      </li>
+                    ))}
+                    {category.valuesInSI.map(value => (
+                      <li className=' font-body font-bold text-16 -mt-10' key={value.unit}>
+                        {value.value}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+        <div className="main-container flex flex-col items-center order-1">
+          <div className=" w-847 translate-y-16">
+            <h4 className=' font-sub text-64 -mb-36'>{carInfo.year}</h4>
+            <h1 className=' font-heading text-64 '>{carInfo.make} {carInfo.model}</h1>
+            <h4 className=' font-sub text-64 float-right -mt-36'>{carInfo.variant}</h4>
+          </div>
+          <div className=' w-full flex justify-center items-start relative'>
+            <div className="">
+              <h4 className=' text-299'>{carInfo.yaer}</h4>
+            </div>
+            <ModelViewer />
+            <div className=" w-500 h-500 bg-first rounded-full absolute -z-10"></div>
+          </div>
         </div>
       </div>
     </>
