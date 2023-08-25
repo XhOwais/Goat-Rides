379fc3853b7fc759bec1db2f8a8e04e5eb26bff9
Xh Owais <87980770+XhOwais@users.noreply.github.com>
2023-08-25 23:59:30 +0500

    Login Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/navbar"
 import Main from "./components/main"
+import Login from "./components/login"
 export default function App() {
   return (
     <>
-    <Main/>
+    {/* <Main/> */}
+    <Login/>
     </>
   )
 }
